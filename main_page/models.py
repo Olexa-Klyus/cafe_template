918359f18e56fce8b66e9143c84bc78066969c7c
olexa <olexakljus@gmail.com>
2023-01-29 11:48:29 +0200

    start commit

--- main_page/models.py
+++ main_page/models.py
@@ -4,3 +4,16 @@ from django.db import models
 class Category(models.Model):
     title = models.CharField(max_length=50, unique=True, db_index=True)
     position = models.SmallIntegerField(unique=True)
+    is_visible = models.BooleanField(default=True)
+
+
+class Dish(models.Model):
+    title = models.CharField(max_length=50, unique=True, db_index=True)
+    position = models.SmallIntegerField()
+    is_visible = models.BooleanField(default=True)
+    ingredients = models.CharField(max_length=255)
+    desc = models.TextField(max_length=500, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    photo = models.ImageField(blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    is_special = models.BooleanField(default=False)
