67a32eb0068946c8f460e9e6077f10c608304e9c
olexa <olexakljus@gmail.com>
2023-01-29 10:33:28 +0200

    start commit

--- main_page/models.py
+++ main_page/models.py
@@ -4,4 +4,3 @@ from django.db import models
 class Category(models.Model):
     title = models.CharField(max_length=50, unique=True, db_index=True)
     position = models.SmallIntegerField(unique=True)
-    
